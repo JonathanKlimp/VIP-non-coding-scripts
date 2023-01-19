fc2623682b597b19af06f9e8ed20491ebfd1492f
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2023-01-19 11:54:24 +0100

    Update on create_plot_control_set.R, changed the column names and added a new score value. Changed the plot to log values

--- plot_scripts/create_plot_control_set.R
+++ plot_scripts/create_plot_control_set.R
@@ -4,9 +4,9 @@ library(reshape2)
 library("optparse")
 
 option_list <- list(
-  make_option(c("-b", "--benign"), type="complex", default=NULL, 
+  make_option(c("-b", "--benign"), type="character", default=NULL, 
               help="input file with the benign variants", metavar="character"),
-  make_option(c("-p", "--pathogenic"), type="complex", default=NULL,
+  make_option(c("-p", "--pathogenic"), type="character", default=NULL,
               help="input file with the pathogenc variants", metavar="character")
 )
 
@@ -36,7 +36,7 @@ gather_scores <- function(all_annotations) {
 all_annotations_benign <- strsplit(ncVarDB_benign$V8, "\\|")
 all_annotations_pathogenic <- strsplit(ncVarDB_patho$V8, "\\|")
 
-scores <- c("0", "1", "2", "3")
+scores <- c("0", "1", "2", "3", "4")
 scores_pathogenic <- as.data.frame(table(gather_scores(all_annotations = all_annotations_pathogenic)))
 scores_benign <- as.data.frame(table(gather_scores(all_annotations = all_annotations_benign)))
 
@@ -45,9 +45,11 @@ df <- data.frame(scores, scores_pathogenic$Freq, scores_benign$Freq)
 colnames(df) <- c("score", "pathogenic", "benign")
 
 score_df <- melt(df)
+names(score_df)[names(score_df) == 'variable'] <- "Variant"
 
-ggplot(score_df, aes(x = score, y= value, fill = variable), ylab="Count") +
+ggplot(score_df, aes(x = score, y= value, fill = Variant), ylab="Count") +
   geom_bar(stat="identity", width=.5, position = "dodge")  +
+  scale_y_log10(breaks=c(0,25,75,150,250,8000)) +
   labs(title="Number of scored variants per score level", y = "Count", x = "Score")
 
 ggsave("Variant_count_per_score.png", device = "png")
