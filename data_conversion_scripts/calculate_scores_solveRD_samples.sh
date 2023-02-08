8f99cccf488579d9184d3acc2b8a8f40ecf0f047
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2023-02-08 09:25:38 +0100

    added call to make a plot of the score annotated file

--- data_conversion_scripts/calculate_scores_solveRD_samples.sh
+++ data_conversion_scripts/calculate_scores_solveRD_samples.sh
@@ -8,5 +8,7 @@ for file in /Users/jonathan/Documents/Afstudeeropdracht/Data/solveRD/vip_annotat
 	gunzip -c $file > tmp.vcf
 	
 	java -jar vcf-decision-tree.jar -i tmp.vcf -o ../score_annotated/$file_name"_annotated.vcf" -f -d
+	
+	Rscript create_plot_scores.R -i ../score_annotated/$file_name"_annotated.vcf" -t "variant scores "$file_name
 
 done 
